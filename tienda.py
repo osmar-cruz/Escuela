6683eb8165ec9ec8b97ae7c6b426a2a969de382c
osmar-cruz <a01715556@tec.mx>
2025-09-13 19:58:26 -0600

    tienda.py
    Here i use the conditionals, like: if, elif and else

--- tienda.py
+++ tienda.py
@@ -4,9 +4,7 @@ taxes=0.16
 tshirts=["leverkusen tshirt","liverpool tshirt","madrid tshirt", "barcelona tshirt", "chivas tshirt"]
 for tshirt in [tshirts]:
     print("-", tshirt)
-quantity = (int(input ("How many tshirts do you want to buy?")))
-total_taxes = quantity * taxes
-print(f"You have to pay", total_taxes, "in taxes for each tshirt")
+
 def compute_discount(quantity): #I define a function to compute the discount based on the quantity of tshirts bought
     if quantity == 5:
         print("You have a 15% discount")
@@ -17,10 +15,33 @@ def compute_discount(quantity): #I define a function to compute the discount bas
     else:
         print("You don't have any discount")
         return 0 #if the quantity is less than 5, there is no discount
+    
+quantity = (int(input ("How many tshirts do you want to buy?")))
+total_taxes = quantity * taxes
+print(f"You have to pay", total_taxes, "in taxes for each tshirt")
+
 discount = compute_discount(quantity) * price * quantity #I call the function to compute the discount and multiply it by the price and quantity to get the total discount amount
 total_price = (price * quantity) + total_taxes - discount 
-"""In this line I calculate the total price with the formula (price * quantity) + total_taxes - discount, and I store it in the variable total_price
-i gave a variable discount to store the discount value for the clients, if you buy 15 or more tshirts you have a 25% discount, if you buy 5 tshirts you have a 15% discount, 
-if you buy less than 5 tshirts you don't have any discount"""
-
+#In this line I calculate the total price with the formula (price * quantity) + total_taxes - discount, and I store it in the variable total_price
 print(f"The total price is: ", total_price)
+
+receipt_option = input("Do you want a receipt? (Y/N): ").lower() #I ask the user if they want a receipt and convert the answer to lowercase
+if receipt_option == "y": #If the answer is yes, I print the receipt
+    while receipt_option.lower() == "y":
+         receipt_option = input("Please enter your email: ")
+    print("----- RECEIPT -----")
+    print(f"Quantity: {quantity}")
+    print(f"Price per tshirt: {price}")
+    print(f"Taxes: {total_taxes}")
+    print(f"Discount: {discount}")
+    print(f"Total price: {total_price}")
+    print("-------------------")    
+    print("The receipt has been sent to your email, Thank you for your purchase!")
+
+elif receipt_option.lower() == "n":
+    print("Thank you for your purchase!")
+
+else:
+    print("Please insert a letter!") #If the answer is no, I thank the user for their purchase
+
+
